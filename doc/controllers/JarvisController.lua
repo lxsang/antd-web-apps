143094d30182483a21362481583eeffdd6798cfd
lxsang <xsang.le@gmail.com>
2021-02-07 13:52:06 +0100

    enable mobile compatibility

--- doc/controllers/JarvisController.lua
+++ doc/controllers/JarvisController.lua
@@ -4,9 +4,5 @@ DocController:subclass("JarvisController", {
         vfs_path = "book://",
         local_path = "/home/mrsang/doc/jarvis",
     },
-    name = "jarvis",
-    elinks = {
-        { name = "API", url = "https://doc.iohub.dev/antos/api/index.html" },
-        { name = "Code", url = "https://github.com/lxsang/antos" }
-    }
+    name = "jarvis"
 })
