4016a0abeff4c5bf3969c2de899914e4ae0d248a
DanyLE <xsang.le@gmail.com>
2022-08-23 01:20:05 +0200

    code refactor

--- os/libs/vfs.lua
+++ os/libs/vfs.lua
@@ -152,7 +152,7 @@ vfs.upload = function(path)
 			local file = m.."/"..REQUEST["upload-"..index..".file"]
 			local ret = ulib.move(REQUEST["upload-"..index..".tmp"], file)
 			if not ret then
-				local ret = ulib.send_file(REQUEST["upload-"..index..".tmp"], file)
+				ret = ulib.send_file(REQUEST["upload-"..index..".tmp"], file)
 			end
 			if not ret then
 			    return false, "Unable to copy file"
